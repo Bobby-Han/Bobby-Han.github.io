9fbb4fee3a274a9e2c57583cdbedbd3c09212144
Bobby-Han <1667827813@qq.com>
2021-07-27 14:57:44 +0800

    Site updated: 2021-07-27 14:57:44

--- libs/codeBlock/codeCopy.js
+++ libs/codeBlock/codeCopy.js
@@ -1,5 +1,21 @@
 // 代码块一键复制
 // 代码块一键复制
+!function (e, t, a) {
+    /* code */
+    var initCopyCode = function () {
+        var copyHtml = '';
+        copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+        copyHtml += '<span>复制</span>';
+        copyHtml += '</button>';
+        $(".highlight .code pre").before(copyHtml);
+        new ClipboardJS('.btn-copy', {
+            target: function (trigger) {
+                return trigger.nextElementSibling;
+            }
+        });
+    }
+    initCopyCode();
+}(window, document);
 $(function () {
     var $copyIcon = $('<i class="fa fa-copy code_copy" title="复制代码" aria-hidden="true"></i>')
     var $notice = $('<div class="codecopy_notice"></div>')
